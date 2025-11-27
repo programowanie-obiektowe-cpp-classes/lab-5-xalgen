5c5bff2c746915bb28b0e13e12b56fa33cfee2dc
Jakub Orlowski <orlowski.jakub9@gmail.com>
2025-11-27 19:00:51 +0100

    zejsciowka

--- foo.hpp
+++ foo.hpp
@@ -1,12 +1,32 @@
-#pragma once
+﻿#pragma once
 
 #include "Human.hpp"
-
 #include <list>
 #include <vector>
+#include <algorithm>
+
 
 std::vector< char > foo(std::list< Human >& people)
 {
-    // Twoja implementacja tutaj
-    return {};
+
+    std::vector<char> result;
+
+    for (auto it = people.begin(); it != people.end(); ++it) {
+
+        it->birthday();
+    }
+
+    for (auto ip = people.begin(); ip != people.end();++ip) {
+
+        if (ip->isMonster()) {
+            result.emplace_back('n');
+
+        }
+        else {
+            result.emplace_back('y');
+        }
+    }
+    std::reverse(result.begin(), result.end());
+
+    return result;
 }
